97b32238061d0a6ce704a6527eea285a92165dc9
hcaula <henrique.caula@hotmail.com>
2017-11-22 14:39:08 -0300

    Add title and title img

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+var gravivity = angular.module('gravivity', []);
+
+// Define the `PhoneListController` controller on the `phonecatApp` module
+gravivity.controller('GravivityController', function GravivityController($scope) {
+  $scope.phones = [
+    {
+      name: 'Nexus S',
+      snippet: 'Fast just got faster with Nexus S.'
+    }, {
+      name: 'Motorola XOOM™ with Wi-Fi',
+      snippet: 'The Next, Next Generation tablet.'
+    }, {
+      name: 'MOTOROLA XOOM™',
+      snippet: 'The Next, Next Generation tablet.'
+    }
+  ];
+});
