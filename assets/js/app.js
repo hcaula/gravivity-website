57265b6e02c5f5d5a6699cf704045f78f3dacb74
hcaula <henrique.caula@hotmail.com>
2017-11-22 18:19:52 -0300

    Stylizing on prototypes page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,9 @@
 var gravivity = angular.module('gravivity', []);
 
-// Define the `PhoneListController` controller on the `phonecatApp` module
 gravivity.controller('GravivityController', function GravivityController($scope) {
   $scope.prototypes = [
     {
-      number: 1,
+      number: "0.1",
       date: new Date("11-29-2017"),
       description: "The first prototype is finally here! In those versions, we want to test certain design choices.",
       latest: {
@@ -13,163 +12,6 @@ gravivity.controller('GravivityController', function GravivityController($scope)
         version: "0.1.0"
       },
       versions: [
-        {
-          version: "0.1.0",
-          iOS: "www.google.com",
-          android: "www.youtube.com",
-          additions: [
-            "Adds main menu",
-            "Adds 15 levels",
-            "Adds new animations and sprites",
-            "Balances levels"
-          ]
-        },
-        {
-          version: "0.1.0",
-          iOS: "www.google.com",
-          android: "www.youtube.com",
-          additions: [
-            "Adds main menu",
-            "Adds 15 levels",
-            "Adds new animations and sprites",
-            "Balances levels"
-          ]
-        },
-        {
-          version: "0.1.0",
-          iOS: "www.google.com",
-          android: "www.youtube.com",
-          additions: [
-            "Adds main menu",
-            "Adds 15 levels",
-            "Adds new animations and sprites",
-            "Balances levels"
-          ]
-        }
-      ]
-    },
-    {
-      number: 1,
-      date: new Date("11-29-2017"),
-      description: "The first prototype is finally here! In those versions, we want to test certain design choices.",
-      latest: {
-        iOS: "www.google.com",
-        android: "www.youtube.com",
-        version: "0.1.0"
-      },
-      versions: [
-        {
-          version: "0.1.0",
-          iOS: "www.google.com",
-          android: "www.youtube.com",
-          additions: [
-            "Adds main menu",
-            "Adds 15 levels",
-            "Adds new animations and sprites",
-            "Balances levels"
-          ]
-        },
-        {
-          version: "0.1.0",
-          iOS: "www.google.com",
-          android: "www.youtube.com",
-          additions: [
-            "Adds main menu",
-            "Adds 15 levels",
-            "Adds new animations and sprites",
-            "Balances levels"
-          ]
-        },
-        {
-          version: "0.1.0",
-          iOS: "www.google.com",
-          android: "www.youtube.com",
-          additions: [
-            "Adds main menu",
-            "Adds 15 levels",
-            "Adds new animations and sprites",
-            "Balances levels"
-          ]
-        }
-      ]
-    },
-    {
-      number: 1,
-      date: new Date("11-29-2017"),
-      description: "The first prototype is finally here! In those versions, we want to test certain design choices.",
-      latest: {
-        iOS: "www.google.com",
-        android: "www.youtube.com",
-        version: "0.1.0"
-      },
-      versions: [
-        {
-          version: "0.1.0",
-          iOS: "www.google.com",
-          android: "www.youtube.com",
-          additions: [
-            "Adds main menu",
-            "Adds 15 levels",
-            "Adds new animations and sprites",
-            "Balances levels"
-          ]
-        },
-        {
-          version: "0.1.0",
-          iOS: "www.google.com",
-          android: "www.youtube.com",
-          additions: [
-            "Adds main menu",
-            "Adds 15 levels",
-            "Adds new animations and sprites",
-            "Balances levels"
-          ]
-        },
-        {
-          version: "0.1.0",
-          iOS: "www.google.com",
-          android: "www.youtube.com",
-          additions: [
-            "Adds main menu",
-            "Adds 15 levels",
-            "Adds new animations and sprites",
-            "Balances levels"
-          ]
-        }
-      ]
-    },
-    {
-      number: 1,
-      date: new Date("11-29-2017"),
-      description: "The first prototype is finally here! In those versions, we want to test certain design choices.",
-      latest: {
-        iOS: "www.google.com",
-        android: "www.youtube.com",
-        version: "0.1.0"
-      },
-      versions: [
-        {
-          version: "0.1.0",
-          iOS: "www.google.com",
-          android: "www.youtube.com",
-          additions: [
-            "Adds main menu",
-            "Adds 15 levels",
-            "Adds new animations and sprites",
-            "Balances levels"
-          ]
-        },
-        {
-          version: "0.1.0",
-          iOS: "www.google.com",
-          android: "www.youtube.com",
-          additions: [
-            "Adds main menu",
-            "Adds 15 levels",
-            "Adds new animations and sprites",
-            "Balances levels"
-          ]
-        },
         {
           version: "0.1.0",
           iOS: "www.google.com",
@@ -185,6 +27,7 @@ gravivity.controller('GravivityController', function GravivityController($scope)
     }
   ];
 
+  var months = ["Jan", "Feb", "Mar", "Apr", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dez"];
   $scope.getBrazilDate = function(date) {
     var s = '';
     s += date.getDate() + '-';
@@ -195,8 +38,14 @@ gravivity.controller('GravivityController', function GravivityController($scope)
 
   $scope.getUSADate = function(date) {
     var s = '';
-    s += date.getMonth() + '-';
-    s += date.getDate() + '-';
+    s += months[date.getMonth()-1] + '. ';
+    var day = date.getDate();
+    s += day;
+    if (day == 1 || day == 21 ) s += "st, ";
+    else if(day == 2 || day == 22) s += "nd, ";
+    else if(day == 3 || day == 23) s += "rd, ";
+    else s+= "th, ";
+
     s += date.getUTCFullYear();
     return s;
   }
