73a65bed26fe4fbde470baa237b45d5b7cf1a0a5
hcaula <henrique.caula@hotmail.com>
2017-11-22 16:02:52 -0300

    Add prototype directive

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,16 +2,40 @@ var gravivity = angular.module('gravivity', []);
 
 // Define the `PhoneListController` controller on the `phonecatApp` module
 gravivity.controller('GravivityController', function GravivityController($scope) {
-  $scope.phones = [
+  $scope.prototypes = [
     {
-      name: 'Nexus S',
-      snippet: 'Fast just got faster with Nexus S.'
-    }, {
-      name: 'Motorola XOOM™ with Wi-Fi',
-      snippet: 'The Next, Next Generation tablet.'
-    }, {
-      name: 'MOTOROLA XOOM™',
-      snippet: 'The Next, Next Generation tablet.'
+      number: 1,
+      date: new Date("11-29-2017"),
+      description: "The first prototype is finally here! In those versions, we want to test certain design choices.",
+      versions: [
+        {
+          version: "0.1.0",
+          iOS: "www.google.com",
+          android: "www.youtube.com",
+          additions: [
+            "Adds main menu",
+            "Adds 15 levels",
+            "Adds new animations and sprites",
+            "Balances levels"
+          ]
+        }
+      ]
     }
   ];
+
+  $scope.getBrazilDate = function(date) {
+    var s = '';
+    s += date.getDate() + '-';
+    s += date.getMonth() + '-';
+    s += date.getUTCFullYear();
+    return s;
+  }
+
+  $scope.getUSADate = function(date) {
+    var s = '';
+    s += date.getMonth() + '-';
+    s += date.getDate() + '-';
+    s += date.getUTCFullYear();
+    return s;
+  }
 });
